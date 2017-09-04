Fixed batch length vector calculation in DSCorpus.get_batch_and_lengths(), incremented all model versions from 1 to 2 signify this fix.

modules/deepspell/corpus.py
@@ -125,15 +125,13 @@ class DSCorpus:
             sample_grammar.random_phrase_with_token(self.data[token_id[0]][token_id[1]])
             for token_id in batch_token_indices]
         # Find the longest phrase, such that all lines in the output matrix can be length-aligned
-        max_phrase_length = max(
+        batch_lengths = np.asarray([
             self._token_sequence_length(phrase_tokens)
-            for phrase_tokens in batch_phrases)
+            for phrase_tokens in batch_phrases])
+        max_phrase_length = max(batch_lengths)
         batch_embedding_sequences = np.asarray([
             self._embed(phrase_tokens, max_phrase_length, min_num_chars_truncate)
             for phrase_tokens in batch_phrases], np.float32)
-        batch_lengths = np.asarray([
-            len(batch_embedding_sequence)
-            for batch_embedding_sequence in batch_embedding_sequences])
         return batch_embedding_sequences, batch_lengths, epoch_leftover_indices
 
     def class_name_for_id(self, id):
@@ -163,7 +161,7 @@ class DSCorpus:
         if not characters[-1] == CHAR_SUBSET[CHAR_SUBSET_EOL]:
             characters += CHAR_SUBSET[CHAR_SUBSET_EOL]
             if classes:
-                classes += [self.eol_class_id]
+                classes += ["EOL"]
         for i, char in enumerate(characters):
             char_id = CHAR_SUBSET_INDEX[char]
             char_embedding = np.zeros(self.total_num_features_per_character())
@@ -190,7 +188,7 @@ class DSCorpus:
             for char in (" " if i > 0 else "")+token.string]
 
         if len(char_class_seq) > min_chars_truncate and min_chars_truncate >= 0:
-            truncation_point = (math.ceil(int(random.uniform(0., 1.) * (len(char_class_seq) - min_chars_truncate))) +
+            truncation_point = (math.ceil(random.uniform(0., 1.) * (len(char_class_seq) - min_chars_truncate)) +
                                 min_chars_truncate)
             char_class_seq = char_class_seq[:truncation_point]
 

modules/deepspell/extrapolator.py
@@ -19,7 +19,13 @@ class DSLstmExtrapolator(predictor.DSPredictor):
 
     def __init__(self, file_or_folder, log_dir="", **kwargs):
         """Documentation in base Model class"""
-        super().__init__("extrapolator", 1, file_or_folder, log_dir, kwargs)
+        super().__init__(
+            name_scope="extrapolator",
+            version=2,
+            file_or_folder=file_or_folder,
+            log_dir=log_dir,
+            kwargs_to_update=kwargs)
+
         # -- Read params
         self.state_size_per_layer = kwargs.pop("state_size_per_layer", [128, 128])
         # -- Create Tensor Flow compute graph nodes
@@ -76,14 +82,16 @@ class DSLstmExtrapolator(predictor.DSPredictor):
         assert self.num_logical_features == completion_corpus.num_logical_features_per_character()
 
         # -- Make sure to reshape the 2D timestep-features matrix into a 3D batch-timestep-features matrix
+        embedded_prefix = completion_corpus.embed_characters(prefix_chars, prefix_classes)
+        embedded_prefix_length = len(embedded_prefix)
         embedded_prefix = np.reshape(
-            completion_corpus.embed_characters(prefix_chars, prefix_classes),
-            newshape=(1, len(prefix_chars), self.num_logical_features + self.num_lexical_features))
+            embedded_prefix,
+            newshape=(1, embedded_prefix_length, self.num_logical_features + self.num_lexical_features))
 
         with self.graph.as_default():
             stepwise_extrapolator_output = self.session.run(self.tf_stepwise_extrapolator_output, feed_dict={
                 self.tf_lexical_logical_embeddings_per_timestep_per_batch: embedded_prefix,
-                self.tf_timesteps_per_batch: np.asarray([len(prefix_chars)]),
+                self.tf_timesteps_per_batch: np.asarray([embedded_prefix_length]),
                 self.tf_maximum_stepwise_extrapolation_length: num_chars_to_predict
             })
         assert len(stepwise_extrapolator_output) == num_chars_to_predict
@@ -162,7 +170,7 @@ class DSLstmExtrapolator(predictor.DSPredictor):
                 one_hot_output = tf.reshape(tf.concat([
                         tf.one_hot(
                             tf.argmax(prev_output[:, :self.num_lexical_features], axis=1),
-                            depth=self.num_logical_features),
+                            depth=self.num_lexical_features),
                         tf.one_hot(
                             tf.argmax(prev_output[:, -self.num_logical_features:], axis=1),
                             depth=self.num_logical_features),