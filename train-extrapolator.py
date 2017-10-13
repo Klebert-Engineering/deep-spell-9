ff8e8df586331048fc3255b2574c7f268f29f794
Joseph Birkner <j.birkner@klebert-engineering.de>
2017-10-13 16:12:15 +0200

    Implementations for refined (lowercase/error robust/natural samples) training.

--- train-extrapolator.py
+++ train-extrapolator.py
@@ -9,7 +9,7 @@ from deepspell.corpus import DSCorpus
 from deepspell.extrapolator import DSLstmExtrapolator
 
 # training_corpus = DSCorpus("corpora/deepspell_minimal.tsv", "na")
-training_corpus = DSCorpus("corpora/deepspell_data_north_america_v2.tsv", "na")
+training_corpus = DSCorpus("corpora/deepspell_data_north_america_nozip_v2.tsv", "na", lowercase=True)
 training_grammar = DSGrammar("corpora/grammar.json", training_corpus.featureset)
 model = DSLstmExtrapolator(
     "models", "logs",
@@ -17,7 +17,7 @@ model = DSLstmExtrapolator(
     learning_rate=0.003,
     learning_rate_decay=0.5,
     training_epochs=5,
-    batch_size=2048,
-    state_size_per_layer=(256, 256))
+    batch_size=3192,
+    state_size_per_layer=(128, 128, 128))
 
 model.train(training_corpus, training_grammar)
