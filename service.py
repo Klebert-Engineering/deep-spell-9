3c2de664774bf92e85bec0a4ac2513dd83ce682c
Joseph Birkner <j.birkner@klebert-engineering.de>
2023-03-24 13:23:58 +0100

    New Version 2023.1, which supports dockerized service with FTS5 lookup.

--- service.py
+++ service.py
@@ -1,8 +1,13 @@
 import json
 import codecs
 import deepspell_service
+import os
 
-with codecs.open("service.json") as config_file:
+config_file_path = "service.json"
+if "SERVICE_CONFIG" in os.environ:
+    config_file_path = os.environ["SERVICE_CONFIG"]
+
+with codecs.open(config_file_path) as config_file:
     params = json.load(config_file)
 
 deepspell_service.views.init(params)
